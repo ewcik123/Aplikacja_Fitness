eda317bc5e4271fc85c770cdf2d1c351c8252e27
DESKTOP-M3PIB3L\ewago <ewa.gowska@gmail.com>
2023-04-25 20:01:16 +0200

    Dodanie pierszych przyciskow

--- FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.fitnessapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fitnessapp.Menu.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //buttonStart.setOnClicklistener{
+          //  goToMenuActivity()
+        //}
+
+    }
+
+    private fun goToMenuActivity() {
+        val activityGoToMenu = Intent(applicationContext, Menu::class.java)
+        startActivity(activityGoToMenu)
     }
 }
---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/Menu/Menu.kt
@@ -0,0 +1,13 @@
+package com.example.fitnessapp.Menu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnessapp.R
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+        
+    }
+}
