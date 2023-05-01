3f1b034b5075ef71bbd52e513d3524398847400a
DESKTOP-M3PIB3L\ewago <ewa.gowska@gmail.com>
2023-05-01 12:53:10 +0200

    Zaprojektowanie widoku dodaj ćwiczenie

---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/AddExercise/AddExercise.kt
@@ -0,0 +1,12 @@
+package com.example.fitnessapp.AddExercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnessapp.R
+
+class AddExercise : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_exercise)
+    }
+}
--- FitnessApp/app/src/main/java/com/example/fitnessapp/Menu/Menu.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/Menu/Menu.kt
@@ -1,7 +1,10 @@
 package com.example.fitnessapp.Menu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.example.fitnessapp.MainActivity
 import com.example.fitnessapp.R
 
 class Menu : AppCompatActivity() {
@@ -9,5 +12,15 @@ class Menu : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
 
+       // findViewById<Button>(R.id.buttonBack).setOnClickListener {
+        //    goToMainActivity()
+        //}
+
     }
+    //private fun goToMainActivity() {
+       // val activityGoMainActivity = Intent(applicationContext, MainActivity::class.java)
+       // startActivity(activityGoMainActivity)
+    //}
 }
+
+
