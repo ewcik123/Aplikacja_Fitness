850db77d3c3d4948d4af248b0a1ecde93fd84fa8
DESKTOP-M3PIB3L\ewago <ewa.gowska@gmail.com>
2023-05-09 19:44:47 +0200

    Utworzenie widoku timer

---
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/RunTraining/RunTraining.kt
@@ -0,0 +1,12 @@
+package com.example.fitnessapp.RunTraining
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fitnessapp.R
+
+class RunTraining : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_run_training)
+    }
+}
