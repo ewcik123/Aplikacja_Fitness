d3b95d8f7f98b550567ab41129f0207bbcb7b168
DESKTOP-M3PIB3L\ewago <ewa.gowska@gmail.com>
2023-05-16 17:54:52 +0200

    zaimplementowanie timera

--- FitnessApp/app/src/main/java/com/example/fitnessapp/Menu/Menu.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/Menu/Menu.kt
@@ -9,6 +9,7 @@ import com.example.fitnessapp.AddExercise.AddExercise
 import com.example.fitnessapp.MainActivity
 import com.example.fitnessapp.Modele.Exercise
 import com.example.fitnessapp.R
+import com.example.fitnessapp.RunTraining.RunTraining
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +17,22 @@ class Menu : AppCompatActivity() {
         setContentView(R.layout.activity_menu)
 
         val buttonClickAddExercise = findViewById<ImageButton>(R.id.buttonAddExcercise)
-            buttonClickAddExercise.setOnClickListener {
-                val intent = Intent(this, AddExercise::class.java)
-                startActivity(intent)
-            }
+        buttonClickAddExercise.setOnClickListener {
+            val intent = Intent(this, AddExercise::class.java)
+            startActivity(intent)
+        }
 
         val buttonClickBack = findViewById<ImageButton>(R.id.buttonBack)
             buttonClickBack.setOnClickListener {
                 val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
             }
+
+        val buttonRunTraining = findViewById<ImageButton>(R.id.buttonRunTraining)
+        buttonRunTraining.setOnClickListener {
+            val intent = Intent(this, RunTraining::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- FitnessApp/app/src/main/java/com/example/fitnessapp/RunTraining/RunTraining.kt
+++ FitnessApp/app/src/main/java/com/example/fitnessapp/RunTraining/RunTraining.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.TextView
 import com.example.fitnessapp.Menu.Menu
 import com.example.fitnessapp.R
 
@@ -20,31 +22,150 @@ class RunTraining : AppCompatActivity() {
 
         val buttonClickStart = findViewById<Button>(R.id.startButton)
         buttonClickStart.setOnClickListener {
-          //  startTimer()
-
+            startTimer()
         }
 
+        val buttonClickStop = findViewById<Button>(R.id.stopButton)
+        buttonClickStop.setOnClickListener {
+            stopTimer()
+        }
 
+        val buttonClickClear = findViewById<Button>(R.id.clearButton)
+        buttonClickClear.setOnClickListener {
+            clearTimer()
+        }
+
+        val buttonAddMinute = findViewById<Button>(R.id.addMinuteButton)
+        buttonAddMinute.setOnClickListener {
+            addMinuteToTimer()
+        }
+
+        val buttonSubstractMinute = findViewById<Button>(R.id.substractMinuteButton)
+        buttonSubstractMinute.setOnClickListener {
+            substractMinuteToTimer()
+        }
+
+        val button15 = findViewById<Button>(R.id.button15s)
+        button15.setOnClickListener {
+            clearTimer()
+            set15()
+        }
+        val button30 = findViewById<Button>(R.id.button30s)
+        button30.setOnClickListener {
+            clearTimer()
+            set30()
+        }
+
+        val button1 = findViewById<Button>(R.id.button1min)
+        button1.setOnClickListener {
+            clearTimer()
+
+        }
 
 
 
     }
 
 
-    //private fun startTimer(){
-        //countDownTimer = object: CountDownTimer(timeLeft.toLong(),1000){
-           // override fun onTick(millisUntilFinished: Long) {
-               // timeLeft = millisUntilFinished.toInt()
-               // updateCountdownText()
-           // }
+    private fun startTimer(){
+        countDownTimer = object: CountDownTimer(timeLeft.toLong(),1000){
+            override fun onTick(millisUntilFinished: Long) {
+                timeLeft = millisUntilFinished.toInt()
+                updateCountdownText()
+            }
 
-           // override fun onFinish(){
-           //     isTimerRunning = false
-             //   updateButtons()
-           // }
-      //  }
-       // countDownTimer.start()
-       // isTimerRunning = true
-       // updateButtons()
-    //}
+           override fun onFinish(){
+                isTimerRunning = false
+                updateButtons()
+            }
+        }
+        countDownTimer.start()
+        isTimerRunning = true
+        updateButtons()
+    }
+    private fun updateCountdownText(){
+        val minutes  =(timeLeft/1000)/60
+        val seconds =(timeLeft/1000) % 60
+        val timeLeftFormated = String.format("%02d:%02d",minutes,seconds)
+        val countdownText = findViewById<TextView>(R.id.countdownText)
+        countdownText.text = timeLeftFormated
+
+    }
+
+    private fun updateButtons()
+    {
+        if( isTimerRunning )
+        {
+            findViewById<Button>(R.id.startButton).isEnabled = false
+            findViewById<Button>(R.id.stopButton).isEnabled = true
+            findViewById<Button>(R.id.clearButton).isEnabled = false
+            findViewById<Button>(R.id.addMinuteButton).isEnabled = false
+            findViewById<Button>(R.id.substractMinuteButton).isEnabled = false
+            findViewById<Button>(R.id.button15s).isEnabled = false
+            findViewById<Button>(R.id.button30s).isEnabled = false
+            findViewById<Button>(R.id.button1min).isEnabled = false
+        }
+        else
+        {
+            findViewById<Button>(R.id.startButton).isEnabled = true
+            findViewById<Button>(R.id.stopButton).isEnabled = false
+            findViewById<Button>(R.id.clearButton).isEnabled = true
+            findViewById<Button>(R.id.addMinuteButton).isEnabled = true
+            findViewById<Button>(R.id.substractMinuteButton).isEnabled = true
+            findViewById<Button>(R.id.button15s).isEnabled = true
+            findViewById<Button>(R.id.button30s).isEnabled = true
+            findViewById<Button>(R.id.button1min).isEnabled = true
+        }
+    }
+    private fun stopTimer()
+    {
+        countDownTimer.cancel()
+        isTimerRunning = false
+        updateButtons()
+    }
+
+    private fun clearTimer()
+    {
+        timeLeft = 60000
+        updateCountdownText()
+        isTimerRunning = false
+        updateButtons()
+    }
+    private fun  addMinuteToTimer()
+    {
+        timeLeft +=60000
+        updateCountdownText()
+        updateButtons()
+
+    }
+    private fun substractMinuteToTimer()
+    {
+        timeLeft -=60000
+        if(timeLeft<0){
+            timeLeft = 0
+        }
+        updateCountdownText()
+        updateButtons()
+    }
+
+
+    private fun set15()
+    {
+        val timeLeftFormated = String.format("%02d:%02d",0,15)
+        val countdownText = findViewById<TextView>(R.id.countdownText)
+        countdownText.text = timeLeftFormated
+        timeLeft = 15000
+
+    }
+
+    private fun set30()
+    {
+        val timeLeftFormated = String.format("%02d:%02d",0,30)
+        val countdownText = findViewById<TextView>(R.id.countdownText)
+        countdownText.text = timeLeftFormated
+        timeLeft = 30000
+
+    }
+
+
 }
